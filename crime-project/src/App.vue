3cfe266461a73e4a5a46e3d134732cedfd296cc2
justthatonedude <jothamgao@gmail.com>
2024-03-05 10:19:42 -0500

    PieChart base

--- crime-project/src/App.vue
+++ crime-project/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -8,11 +7,10 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/piechart">PieChart</RouterLink>
       </nav>
     </div>
   </header>
@@ -83,3 +81,4 @@ nav a:first-of-type {
   }
 }
 </style>
+./components/PieChart.vue
---
+++ crime-project/src/components/chartData/chartConfig.js
@@ -0,0 +1,16 @@
+import {filter} from '@'
+
+export const data = {
+    labels: filter,
+    datasets: [
+      {
+        backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+        data: [40, 20, 80, 10]
+      }
+    ]
+  }
+  
+  export const options = {
+    responsive: true,
+    maintainAspectRatio: false
+  }
