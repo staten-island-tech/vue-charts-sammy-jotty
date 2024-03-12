19ddf9527ee0d883d916cf828296e96244ab95e3
Samiksusamik <sam.kipnis@gmail.com>
2024-03-12 10:15:37 -0400

    e

---
+++ crime-project/src/views/FilterSelectors.vue
@@ -0,0 +1,11 @@
+<script setup>
+import DataFilterSelect from '@/components/DataFilterSelect.vue'
+import DataFilterCheckbox from '@/components/DataFilterCheckbox.vue'
+</script>
+
+<template>
+  <div>
+    <!-- <DataFilterSelect :options="['WHITE', 'BLACK', 'HISPANIC']" type="perp_race" /> -->
+    <DataFilterCheckbox type="perp_race" />
+  </div>
+</template>
