4ae34287e6ef15c45a6578e7785d89902f7effe3
justthatonedude <jothamgao@gmail.com>
2024-03-12 10:21:03 -0400

    Revised ChartConfig

--- crime-project/src/components/chartData/chartConfig.js
+++ crime-project/src/components/chartData/chartConfig.js
@@ -1,26 +1,30 @@
 import applyFilters from '../../api/applyMultipleFilters.js'
 import getAllData from '../../api/getAllData.js'
 const crime_data = await getAllData()
-const filters = [{
-  type:'perp_race',
-  value:'black',
-}]
+const filtersObj = [
+  {
+    type:'age_group',
+    values:['18-24','25-44','45-64']
+  }
+]
+
+const filters = filtersObj.forEach((value)=>value.values.forEach((filters)=>filters))
 const filteredData = await applyFilters(crime_data,filters)
 
-const labels = []
 
-filters.forEach((filter)=>labels.push(filter.value))
 export const data = {
-  labels: [labels],
+  labels: [],
   datasets: [
     {
       label:['Arrests'],
-      backgroundColor: ['#E46651','#246655'],
-      data: [filteredData.length]
+      backgroundColor: ['#E46651','#246655','#E33655'],
+      data: [filteredData.length,filteredData.length,filteredData.length]
     },
+    
   ]
 }
-
+filtersObj.forEach((filter)=>filter.values.forEach((value)=>data.labels.push(value)))
+/* data.data.push(filteredData.length) */
 export const options = {
   responsive: true,
   maintainAspectRatio: false
