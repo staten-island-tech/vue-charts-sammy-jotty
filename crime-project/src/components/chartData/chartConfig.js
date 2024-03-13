34152fa7447a106681499ffd3cba89aa10ef8100
justthatonedude <jothamgao@gmail.com>
2024-03-13 10:25:23 -0400

    update chartConfig.js

--- crime-project/src/components/chartData/chartConfig.js
+++ crime-project/src/components/chartData/chartConfig.js
@@ -1,14 +1,13 @@
 import applyFilters from '../../api/applyMultipleFilters.js'
 import getAllData from '../../api/getAllData.js'
 const crime_data = await getAllData()
-const filtersObj = [
+const filters = [
   {
     type:'age_group',
-    values:['18-24','25-44','45-64']
+    values:['18-24','25-44','45-64'],
   }
 ]
 
-const filters = filtersObj.forEach((value)=>value.values.forEach((filters)=>filters))
 const filteredData = await applyFilters(crime_data,filters)
 
 
@@ -18,13 +17,12 @@ export const data = {
     {
       label:['Arrests'],
       backgroundColor: ['#E46651','#246655','#E33655'],
-      data: [filteredData.length,filteredData.length,filteredData.length]
+      data: [filteredData.length]
     },
     
   ]
 }
-filtersObj.forEach((filter)=>filter.values.forEach((value)=>data.labels.push(value)))
-/* data.data.push(filteredData.length) */
+filters.forEach((filter)=>filter.values.forEach((value)=>data.labels.push(value)))
 export const options = {
   responsive: true,
   maintainAspectRatio: false
