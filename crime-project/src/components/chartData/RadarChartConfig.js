0ae03ffd1d24884c8aacf2f20f5506250b0d60bc
justthatonedude <jothamgao@gmail.com>
2024-03-28 09:34:01 -0400

    final chart revisions

--- crime-project/src/components/chartData/RadarChartConfig.js
+++ crime-project/src/components/chartData/RadarChartConfig.js
@@ -14,7 +14,7 @@ const filteredData = await applyFilters(crime_data,filters)
 const decompressedData = decompressData(filteredData, filterType)
 console.log(decompressedData)
 export const data = {
-  labels: Object.keys(decompressedData),
+  labels: ['Bronx','Staten Island','Brooklyn','Queens','Manhattan'],
   datasets: [
     {
       label: 'Arrests',
--- crime-project/src/views/HomeView.vue
+++ crime-project/src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <PieChart/>
   </div>
 </template>
 
